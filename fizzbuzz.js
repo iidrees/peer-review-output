cd97aabbce21fb3cf13223176afdd7fe37b3484d
idrees <idreesibraheem@gmail.com>
2018-04-07 16:33:08 +0100

    test: add implementation of test - add eslint rules to ensure es6 airbnb rules are adhered to - ensure all tests pass

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,19 @@
 /* export function */
 
 const FizzBuzz = function(n) {
-  
+  if (typeof n !== 'number') {
+    return 'Please enter a number';
+  } else if (n < 0 ) {
+    return 'Number must be positive'
+  } else if (n % 3 !== 0 && n % 5 !== 0) {
+    return n;
+  } else if (n % 3 === 0 && n % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (n % 5 === 0) {
+    return 'buzz'
+  } else if (n % 3 === 0 ) {
+    return 'fizz'
+  }
 }
 
 export default FizzBuzz;
--- squared.js
+++ squared.js
@@ -1,13 +1,20 @@
-/* Test */
-
-
-
+/* function */
 
 const squareEachNumber = (number) => {//args
 
-  
+  let squared= "";// initialise the square variable
+
+  if(typeof number !== 'number') {// conditon to check if input is a number
+  return 'Please enter an integer'
+  } else if (number < 1) {// conditon to check if input is not zero or negative
+    return 'Please enter a number greater than 1'
+  } 
+
+  const arrayNum = [...number + ''].map( n => {// input is squared here
+    squared += parseInt(n) * parseInt(n)
+  })
+  return Number(squared);
 } 
 
-squareEachNumber(44);
 
-export default squareEachNumber;
+export default squareEachNumber;// export function
